db6b3b169a8c9de04c43dab3e642b2ebb449e04e
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2021-11-22 17:26:52 +0900

    feat: UserのCRUD画面を追加

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,40 @@
+class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    print('params:', user_params)
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_user_url, notice: "ユーザー「#{@user.name}」を削除しました。"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+end
