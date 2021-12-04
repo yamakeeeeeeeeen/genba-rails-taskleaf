3d452144356dc067c28f86e898f77ff8729b0760
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2021-12-04 19:50:52 +0900

    feat: ログインしているユーザーのTaskデータの登録

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
 
     if @task.save
       redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
